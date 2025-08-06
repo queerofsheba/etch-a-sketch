9dae858dcf73879da91b3a5304548924a2c24c4c
Ruby <Home@RBMacBookPro.attlocal.net>
2025-08-06 14:31:07 -0700

    Add button for user-input-generated grid

--- javascript.js
+++ javascript.js
@@ -1,16 +1,22 @@
 // ACCESS & STYLE the div component of html document
 const container = document.querySelector("#flex-container");
-container.setAttribute("style", "display: flex; background-color: coral; justify-content: center; padding: 10px; flexGrow: 1; flex-direction: column; align-items: center;")
+container.setAttribute("style", "display: flex; height: 400; background-color: coral; justify-content: center; padding: 10px; flexGrow: 1; flex-direction: column; align-items: center;")
 
-// CREATE 16 square div elements in a single row
-function createRow () {
+// CREATE specified # square div elements with hover interaction in a single row
+function createRow (number) {
 let i = 0;
 let row = document.createElement("div");
 row.setAttribute("style", "display: flex;")
-while (i < 16) {
+while (i < number) {
     const square = document.createElement("div");
     square.classList.add("grid");
-    square.setAttribute("style", "flex: none; margin: 4px; height: 50px; width: 50px; background-color: white;");
+    square.setAttribute("style", "flex: none; margin: 4px; height: 40px; width: 40px; background-color: white;");
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "coral"
+    })
+    square.addEventListener("mouseleave", () => {
+        square.style.backgroundColor = "red"
+    })
     row.appendChild(square);
     i++;
 }
@@ -18,30 +24,31 @@ container.appendChild(row);
 }
 
 // CALL function for 16 single rows of 16 square div elements
-row01 = createRow();
-row02 = createRow();
-row03 = createRow();
-row04 = createRow();
-row05 = createRow();
-row06 = createRow();
-row07 = createRow();
-row08 = createRow();
-row09 = createRow();
-row10 = createRow();
-row11 = createRow();
-row12 = createRow();
-row13 = createRow();
-row14 = createRow();
-row15 = createRow();
-row16 = createRow();
+row01 = createRow(16);
+row02 = createRow(16);
+row03 = createRow(16);
+row04 = createRow(16);
+row05 = createRow(16);
+row06 = createRow(16);
+row07 = createRow(16);
+row08 = createRow(16);
+row09 = createRow(16);
+row10 = createRow(16);
+row11 = createRow(16);
+row12 = createRow(16);
+row13 = createRow(16);
+row14 = createRow(16);
+row15 = createRow(16);
+row16 = createRow(16);
 
-// ADD 'hover' effect to each square element
-const grid = document.querySelectorAll(".grid");
-for (const square of grid) {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "coral"
-    })
-    square.addEventListener("mouseleave", () => {
-        square.style.backgroundColor = "red"
-    })
-}
+// EMPTY container & PROMPT user to enter number for new grid
+const btn = document.querySelector(".reset")
+btn.addEventListener("click", () => {
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    };
+    let num = prompt("Enter a number between 1 and 100", "16");
+    for (let i = 0; i < num; i++) {
+        createRow(num);
+    }
+})
